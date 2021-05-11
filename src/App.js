75cf8f0fcb6eb7a0327231a83f1123a089473610
Muhammad Muaz <49091541+hafi51@users.noreply.github.com>
2021-05-11 17:21:52 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from "react";
+import "./App.css";
+import useWebAnimations from "@wellyshen/use-web-animations";
 
 function App() {
+  const Alice_Queen = useRef();
+  const { ref, playState } = useWebAnimations({
+    keyframes: [
+      { transform: "translate(0px) " },
+      { transform: "translate(500px) " },
+    ],
+    timing: {
+      duration: 2000,
+      iterations: Infinity,
+      direction: "alternate",
+    },
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="sky"></div>
+      <div className="earth">
+        <div className="scenery">
+          <img
+            id="palm1"
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1_small.png"
+          />
+          <img
+            id="pawn1"
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png"
+          />
+          <img
+            id="rook1"
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_small.png"
+          />
+        </div>
+        <div id="#alice">
+          <img
+            ref={ref}
+            id="alice_queen"
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png"
+          />
+        </div>
+      </div>
     </div>
   );
 }
